af8122efef2ddff8a076414201e1b40065eef099
mbuergle <buergler@vaw.baug.ethz.ch>
2021-05-27 08:43:20 +0200

    improved time series analysis

--- velocity_tsa.py
+++ velocity_tsa.py
@@ -13,6 +13,7 @@ import time
 import math
 import pathlib
 from statsmodels.tsa.stattools import acf, pacf
+from scipy import signal
 from matplotlib import pyplot as plt
 try:
     from dataio.H5Writer import H5Writer
@@ -59,7 +60,7 @@ ts_frequency = int((len(t)-1)/total_duration)
 fs1=7
 dUdt_vel = np.diff(vel,axis=0)*ts_frequency
 log10_first = 1
-resampling_frequencies = np.logspace(log10_first,math.log10(ts_frequency),int(math.log10(ts_frequency)))
+resampling_frequencies = np.flip(np.logspace(log10_first,math.log10(ts_frequency),int(math.log10(ts_frequency))))
 fig, axs = plt.subplots(len(resampling_frequencies),len(directions),figsize=(len(directions)*2.5,len(resampling_frequencies)*2.5))
 for ii,res_frq in enumerate(resampling_frequencies):
     n_timesteps = res_frq*total_duration
@@ -72,6 +73,7 @@ for ii,res_frq in enumerate(resampling_frequencies):
         axs[ii,jj].set_title(r'$\tau$ = {0} s'.format(1/res_frq))
         axs[ii,jj].text(0.99*min(dUdt_vel.reshape(-1)),0.95*max(n),r'$\mu$ = {:.1f}m/s$^{{-2}}$'.format(np.mean(dUdt[:,jj])),fontsize=fs1)
         axs[ii,jj].text(0.99*min(dUdt_vel.reshape(-1)),0.85*max(n),r'$\sigma$ = {:.0f}m/s$^{{-2}}$'.format(np.std(dUdt[:,jj])),fontsize=fs1)
+        axs[ii,jj].set_xlim([-max(abs(bins)),max(abs(bins))])
         if jj == 0:
             axs[ii,jj].set_ylabel('Frequency [-]')
         if ii == len(resampling_frequencies)-1:
@@ -87,8 +89,8 @@ fig.savefig(path / 'acceleration_histrograms_xyz.svg',dpi=300)
 # Accelrations for various sampling frequencies
 fs1=7
 log10_first = 1
-resampling_frequencies = np.logspace(log10_first,math.log10(ts_frequency),int(math.log10(ts_frequency)))
-fig, axs = plt.subplots(ncols=len(resampling_frequencies),figsize=(len(resampling_frequencies)*2.5,4))
+resampling_frequencies = np.flip(np.logspace(log10_first,math.log10(ts_frequency),int(math.log10(ts_frequency))))
+fig, axs = plt.subplots(ncols=len(resampling_frequencies),figsize=(len(resampling_frequencies)*2.5,2.5))
 for jj,res_frq in enumerate(resampling_frequencies):
     n_timesteps = res_frq*total_duration
     res_indices = np.linspace(0,len(t)-1,int(n_timesteps)+1).astype('int')
@@ -104,15 +106,46 @@ for jj,res_frq in enumerate(resampling_frequencies):
     if jj == 0:
         axs[jj].set_ylabel('Frequency [-]')
     axs[jj].set_xlabel(r'Acceleration $dU_x/dt$ [m/s$^{-2}$]')
+    axs[jj].set_xlim([-max(abs(bins)),max(abs(bins))])
 plt.tight_layout()
 fig.savefig(path / 'acceleration_histrograms_x.svg',dpi=300)
+
+# Accelrations for various sampling frequencies
+fs1=7
+fig = plt.figure(figsize=(3.0,2.5))
+dU = np.diff(vel,axis=0)
+dUdt = dU*ts_frequency
+(n, bins, patches) = plt.hist(dUdt[:,0],bins=100 ,density=True, \
+        range=(min(dUdt_vel.reshape(-1)),max(dUdt_vel.reshape(-1))))
+plt.title(r'$\delta t$ = {0} s'.format(1/ts_frequency))
+plt.text(0.99*min(dUdt_vel.reshape(-1)),0.95*max(n),r'$\mu$ = {:.1f}m/s$^{{-2}}$'.format(np.mean(dUdt[:,0])),fontsize=fs1)
+plt.text(0.99*min(dUdt_vel.reshape(-1)),0.85*max(n),r'$\sigma$ = {:.0f}m/s$^{{-2}}$'.format(np.std(dUdt[:,0])),fontsize=fs1)
+plt.ylabel('Frequency [-]')
+plt.xlabel(r'Acceleration $dU_x/dt$ [m/s$^{-2}$]')
+plt.xlim([-max(abs(bins)),max(abs(bins))])
+plt.tight_layout()
+fig.savefig(path / 'acceleration_histrograms_x_single.svg',dpi=300)
 
 fs2=10
 T_L = flow_props['integral_timescale']
 T_I = flow_props['turbulent_intensity']
 U_m = flow_props['mean_velocity']
-directions = ['x']
-fig = plt.figure(figsize=(6,4))
+# Plot velocity time series
+color = [0.2,0.2,0.2]
+lw = 0.2
+fig = plt.figure(figsize=(4.5,2.5))
+plt.plot(t,vel[:,0],color=color,lw=lw,label='simulated, Langevin eq.')
+plt.ylabel('$u_x$ [m/s]')
+plt.xlabel('time $t$ [s]')
+plt.xlim([0,flow_props['duration']])
+plt.ylim([U_m[0]-5*U_m[0]*T_I[0],U_m[0]+5*U_m[0]*T_I[0]])
+plt.title(r'$T_{{I,x}}$ = {}, $T_{{L,x}}$ = {}s, $dt$={}s, $T_{{L,x}}/dt$={}'.format(T_I[0],T_L[0],1.0/ts_frequency,int(T_L[0]*ts_frequency)),fontsize=fs2)
+plt.grid(which='major', axis='both')
+plt.legend(loc=2)
+plt.tight_layout()
+fig.savefig(path / 'velocity_ts.svg',dpi=300)
+
+fig = plt.figure(figsize=(4.5,2.5))
 plt.plot(t/T_L[0],(vel[:,0]-U_m[0])/(U_m[0]*T_I[0]),color='k', lw=0.75, \
     label='simulated, Langevin eq.')
 plt.xlim([0,10])
@@ -121,15 +154,13 @@ plt.xlabel('$t/T_L$ [-]')
 plt.ylabel(r'$(u*-\bar{u*})/ \sigma$ [-]')
 plt.legend(loc=2)
 plt.grid(which='major', axis='both')
-plt.text(8,3.7,r'$T_L$ = {}s'.format(T_L[0]),fontsize=fs2)
-plt.text(8,3.3,r'$dt$ = {}s'.format(1.0/ts_frequency),fontsize=fs2)
-plt.text(8,2.9,r'$T_L/dt$ = {}'.format(int(T_L[0]*ts_frequency)),fontsize=fs2)
+plt.title(r'$T_{{I,x}}$ = {}, $T_{{L,x}}$ = {}s, $dt$={}s, $T_{{L,x}}/dt$={}'.format(T_I[0],T_L[0],1.0/ts_frequency,int(T_L[0]*ts_frequency)),fontsize=fs2)
 plt.tight_layout()
 fig.savefig(path /'normalized_velocity_ts.svg',dpi=300)
 
 lag = int(5*T_L[0]*ts_frequency)
 acf,ci = acf(vel[:,0],nlags=lag,alpha=0.05,fft=False)
-fig = plt.figure(figsize=(6,4))
+fig = plt.figure(figsize=(4.5,2.5))
 plt.plot(np.linspace(0,lag,lag+1)/ts_frequency/T_L[0],acf,color='k',linestyle='-',label='simulated, Langevin eq.')
 # plt.plot(np.linspace(0,lag,lag+1),ci,color='k',linestyle=':')
 plt.plot(np.linspace(0,lag,lag+1)/ts_frequency/T_L[0],np.exp(-np.linspace(0,lag,lag+1)/ts_frequency/T_L[0]),color='k',linestyle=':',label='theoritical')
@@ -141,3 +172,12 @@ plt.legend(loc=1)
 plt.grid(which='major', axis='both')
 plt.tight_layout()
 fig.savefig(path / 'acf.svg',dpi=300)
+
+freqs, psd = signal.welch(vel[:,0], fs=ts_frequency)
+fig = plt.figure(figsize=(4.5,2.5))
+plt.loglog(freqs, psd)
+plt.title('PSD: power spectral density')
+plt.xlabel('Frequency')
+plt.ylabel('Power')
+plt.tight_layout()
+fig.savefig(path / 'psd.svg',dpi=300)
